b4558f149d739f3b0c38c6fe931edcb02be754f4
Bruno Rosendo <brunomrosendo@hotmail.com>
2021-03-30 17:28:23 +0100

    Reading keys

--- project/MyScene.js
+++ project/MyScene.js
@@ -67,7 +67,7 @@ export class MyScene extends CGFscene {
 
     // called periodically (as per setUpdatePeriod() in init())
     update(t){
-        //To be done...
+        this.checkKeys();
     }
 
     display() {
@@ -95,4 +95,23 @@ export class MyScene extends CGFscene {
 
         // ---- END Primitive drawing section
     }
+
+    checkKeys()  {
+        var text = "Keys pressed: ";
+        var keysPressed = false;
+
+        // Check for key codes e.g. in https://keycode.info/
+        if (this.gui.isKeyPressed("KeyW")) {
+                text += " W ";
+                keysPressed = true;
+        }
+
+        if (this.gui.isKeyPressed("KeyS")) {
+                text += " S ";
+                keysPressed = true;
+        }
+
+        if (keysPressed)
+                console.log(text);
+  }
 }
