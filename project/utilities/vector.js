1dd9686e4b4d9c6d60731d1e43a1714ce3615bb4
Henrique Sousa <up201906681@fe.up.pt>
2021-03-30 17:34:50 +0100

    moving object

---
+++ project/utilities/vector.js
@@ -0,0 +1,17 @@
+export function norm3d(vec3){
+    return Math.sqrt(ve3[0]**2 + vec3[1]**2 + vec3[2]**2);
+}
+
+export function rotate3dByAxis(vec3, angle, axis==1){
+    if(axis == 0){
+        //sum of cosines
+        cos, sin = vec3[0], vec[2]
+    }
+    else if(axis ==1){
+        cos, sin = vec3[0], vec[2]
+    }
+    else if(axis == 2){
+        cos, sin = vec3[0], vec[2]
+    }
+    return vec3;
+}
