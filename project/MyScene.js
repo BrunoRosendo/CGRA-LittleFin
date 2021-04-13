1cc057ee414ca4d8440106ab8faccec44f6240e7
Henrique Sousa <up201906681@fe.up.pt>
2021-04-13 16:36:11 +0100

    sphere with texture working

--- project/MyScene.js
+++ project/MyScene.js
@@ -63,9 +63,9 @@ export class MyScene extends CGFscene {
 
         // Initialize scene objects
         this.axis = new CGFaxis(this);
-        this.incompleteSphere = new MySphere(this, 16, 8);
         this.pyramid = new MyMovingObject(this, new MyPyramid(this, 10, 3));
         this.mycubemap = new MyCubeMap(this, this.cubeMaptextures[this.selectedTexture]);
+        this.sphere = new MySphere(this, 16, 8);
 
 
         this.defaultAppearance = new CGFappearance(this);
@@ -87,13 +87,15 @@ export class MyScene extends CGFscene {
         this.sphereAppearance.setDiffuse(0.7, 0.7, 0.7, 1);
         this.sphereAppearance.setSpecular(0.0, 0.0, 0.0, 1);
         this.sphereAppearance.setShininess(120);
+        this.sphereAppearance.loadTexture('./images/earth.jpg');
 
 
         // Objects connected to MyInterface
         this.displayAxis = true;
         this.displaySphere = false;
         this.displayPyramid = false;
-        this.displayMyCubeMap = true;
+        this.displayMyCubeMap = false;
+        this.displaySphere = true;
     }
     initLights() {
         this.lights[0].setPosition(15, 2, 5, 1);
@@ -152,7 +154,7 @@ export class MyScene extends CGFscene {
         // This sphere does not have defined texture coordinates
         if (this.displaySphere) {
             this.sphereAppearance.apply();
-            this.incompleteSphere.display();
+            this.sphere.display();
         }
 
         if (this.displayMyCubeMap) {
@@ -160,6 +162,7 @@ export class MyScene extends CGFscene {
             this.scale(50,50,50);
             this.mycubemap.display();
         }
+
 
         // ---- END Primitive drawing section
     }
