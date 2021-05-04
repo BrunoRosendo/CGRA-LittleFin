517a195de91f4df55aaa622c1a7c56acb32e20a4
Bruno Rosendo <brunomrosendo@hotmail.com>
2021-05-04 01:18:44 +0100

    Separated pillar from pier

--- project/MyPier.js
+++ project/MyPier.js
@@ -1,57 +1,37 @@
 import {CGFappearance} from '../lib/CGF.js';
-import { MyCylinder } from "./MyCylinder.js";
+import { MyPillar } from "./MyPillar.js";
 
 export class MyPier {
     constructor(scene, width, length, height, xPosition, yPosition, zPosition) {
         this.scene = scene;
-        this.cyllinder = new MyCylinder(scene, 20);
-        this.width = width;
+        this.cyllinder = new MyPillar(scene, 20, width, length, height);
+
         this.length = length;
-        this.height = height;
+        this.width = width;
         this.xPosition = xPosition;
         this.yPosition = yPosition;
         this.zPosition = zPosition;
 
-        this.material = new CGFappearance(scene);
-        this.material.setAmbient(1.0, 1.0, 1.0, 1.0);
-        this.material.setDiffuse(1.0, 1.0, 1.0, 1.0);
-        this.material.setSpecular(1.0, 1.0, 1.0, 1.0);
-        this.material.setShininess(5);
-        this.material.loadTexture('./images/earth.jpg');
     }
 
     display() {
-        this.material.apply();
         this.scene.translate(this.xPosition, this.yPosition, this.zPosition);
         this.scene.pushMatrix();
 
         //FrontRight
         this.scene.translate(this.width/2,0,this.length/2);
-        this.scene.scale(1,this.height,1);
         this.cyllinder.display();
-        this.scene.popMatrix();
-        this.scene.pushMatrix();
 
         //BackRight
         this.scene.translate(this.width/2,0,-this.length/2);
-        this.scene.scale(1,this.height,1);
         this.cyllinder.display();
-        this.scene.popMatrix();
-        this.scene.pushMatrix();
 
         //FrontLeft
         this.scene.translate(-this.width/2,0,this.length/2);
-        this.scene.scale(1,this.height,1);
         this.cyllinder.display();
-        this.scene.popMatrix();
-        this.scene.pushMatrix();
 
         //BackLeft
         this.scene.translate(-this.width/2,0,-this.length/2);
-        this.scene.scale(1,this.height,1);
         this.cyllinder.display();
-        this.scene.popMatrix();
-        this.scene.pushMatrix();
-        
     }
 }
--- project/MyPillar.js
+++ project/MyPillar.js
@@ -1,49 +1,34 @@
-import {CGFobject, CGFappearance} from '../lib/CGF.js';
+import { CGFobject, CGFappearance } from '../lib/CGF.js';
+import { MyCylinder } from './MyCylinder.js';
 
-export class MyCylinder extends CGFobject {
+export class MyPillar extends CGFobject {
   /**
    * @method constructor
    * @param  {CGFscene} scene - MyScene object
    * @param  {integer} slices - number of slices around Y axis
    */
-  constructor(scene, slices) {
+  constructor(scene, slices, height) {
     super(scene);
-    this.slices = slices;
+    this.cylinder = new MyCylinder(scene, slices);
 
-    this.initBuffers();
+    this.height = height;
+
+    this.material = new CGFappearance(scene);
+    this.material.setAmbient(1.0, 1.0, 1.0, 1.0);
+    this.material.setDiffuse(1.0, 1.0, 1.0, 1.0);
+    this.material.setSpecular(1.0, 1.0, 1.0, 1.0);
+    this.material.setShininess(5);
+    this.material.loadTexture('./images/underwater/tree.jpeg');
   }
 
-  initBuffers() {
-    this.vertices = [];
-    this.indices = [];
-    this.normals = [];
-    this.texCoords = [];
+  display() {
+    this.material.apply();
 
-    const deltaTheta = 2 * Math.PI / this.slices;
-    for (let theta = 0, i = 0; i <= 2 * this.slices; theta += deltaTheta, i += 2) {
-        // Vertices coordinates
-        const x = Math.sin(theta);
-        const z = Math.cos(theta);
+    this.scene.scale(1, this.height, 1);
+    this.cylinder.display();
 
-        this.vertices.push(x, 0, z);
-        this.vertices.push(x, 1, z);
-
-        // Indices
-        if (i < 2 * this.slices) {  // last slice does not need indices
-          this.indices.push(i + 2, i + 1, i);
-          this.indices.push(i + 1, i + 2, i + 3);
-        }
-
-        // Normals
-        this.normals.push(x, 0, z);
-        this.normals.push(x, 0, z);
-
-        // Tex Coords
-        this.texCoords.push(i / (2 * this.slices), 1);
-        this.texCoords.push(i / (2 * this.slices), 0);
-    }
-
-    this.primitiveType = this.scene.gl.TRIANGLES;
-    this.initGLBuffers();
+    this.scene.popMatrix();
+    this.scene.pushMatrix();
+    this.scene.defaultAppearance.apply();
   }
 }
