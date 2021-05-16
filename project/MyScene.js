2bbd6c4047213ec00421dd734d6932cc059a1f0f
Bruno Rosendo <brunomrosendo@hotmail.com>
2021-05-16 01:21:59 +0100

    More Screenshots

--- project/MyScene.js
+++ project/MyScene.js
@@ -144,7 +144,6 @@ export class MyScene extends CGFscene {
     }
 
     checkKeys() {
-        /*
         // Check for key codes e.g. in https://keycode.info/
         if (this.gui.isKeyPressed("KeyW")) {
             this.fish.accelerate(this.moveSpeed);
@@ -188,7 +187,7 @@ export class MyScene extends CGFscene {
                     this.fish.position[2]
                 );
         }
-*/
+
     }
 
     display() {
